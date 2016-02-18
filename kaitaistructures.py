f3687b849dd01e9af32e6786ed6a813448cf8a38
Mikhail Yakshin <greycat@altlinux.org>
2016-02-18 17:58:18 +0300

    Fix formatting as per PEP8

--- kaitaistructures.py
+++ kaitaistructures.py
@@ -1,5 +1,8 @@
 from struct import unpack
 
 class KaitaiStruct:
-  def read_u1(self):
-    return unpack('B', self._io.read(1))[0]
+    def close(self):
+        self._io.close()
+
+    def read_u1(self):
+        return unpack('B', self._io.read(1))[0]
