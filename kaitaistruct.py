0c6719ad29cd87841cd87fef5c3f785cdc5d8478
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2016-10-11 14:44:59 +0300

    Revert "Convert xor operations to one liners"
    This reverts commit 04289cc13d6501153cab61e875d9c4e2ccdaa5d8.

--- kaitaistruct.py
+++ kaitaistruct.py
@@ -232,11 +232,23 @@ class KaitaiStream:
 
     @staticmethod
     def process_xor_one(data, key):
-        return bytes(bytearray(v ^ key for v in bytearray(data)))
+        r = bytearray(data)
+        for i in range(len(r)):
+            r[i] ^= key
+        return bytes(r)
 
     @staticmethod
     def process_xor_many(data, key):
-        return bytes(bytearray(a ^ b for a, b in map(bytearray, zip(data, key))))
+        r = bytearray(data)
+        k = bytearray(key)
+        ki = 0
+        kl = len(k)
+        for i in range(len(r)):
+            r[i] ^= k[ki]
+            ki += 1
+            if ki >= kl:
+                ki = 0
+        return bytes(r)
 
     @staticmethod
     def process_rotate_left(data, amount, group_size):
