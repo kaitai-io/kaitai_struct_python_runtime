7c486913c1518eb5ba5a4f2ce5a4f54cf2ea5c2a
Mikhail Yakshin <greycat@altlinux.org>
2018-02-06 00:48:41 +0000

    Bump version

--- kaitaistruct.py
+++ kaitaistruct.py
@@ -12,7 +12,7 @@ PY2 = sys.version_info[0] == 2
 #   KS runtime library by this version number;
 # * distribution utils (setup.py) use this when packaging for PyPI
 #
-__version__ = '0.7'
+__version__ = '0.8'
 
 
 class KaitaiStruct(object):
